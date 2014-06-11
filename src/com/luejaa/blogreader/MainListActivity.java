057d6cd0d5069d03eedde51d86f437c7b5c4cb4a
Alonso Gutierrez <darkspace20@hotmail.com>
2014-06-10 21:50:43 -0400

    Initialized git, and added all project files

---
+++ src/com/luejaa/blogreader/MainListActivity.java
@@ -0,0 +1,224 @@
+package com.luejaa.blogreader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainListActivity extends ListActivity {
+
+	//protected String[] mBlogPostTitles;
+	public static final int NUMBER_OF_POSTS = 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
+	protected JSONObject mBlogData;
+	protected ProgressBar mProgressBar;
+	private final String KEY_TITLE = "title";
+	private final String KEY_AUTHOR = "author";
+	
+	//on create class, call also progress bar, check if network is available. And GetBlogPostsTask 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_list);
+		
+		mProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+		
+		if(isNetworkAvailable()){
+			mProgressBar.setVisibility(View.VISIBLE);
+			GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+			getBlogPostsTask.execute();
+		}else{
+			Toast.makeText(this, "Network is unavailable", Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {		
+		super.onListItemClick(l, v, position, id);
+		JSONArray jsonPosts;
+		try {
+			jsonPosts = mBlogData.getJSONArray("posts");
+			JSONObject jsonPost = jsonPosts.getJSONObject(position);
+			String blogUrl = jsonPost.getString("url");
+			
+			//=========== we are using an Intent object here!
+			Intent intent = new Intent(this, BlogWebViewActivity.class); //explicit intent to open blogwebview
+			intent.setData(Uri.parse(blogUrl));
+			startActivity(intent);
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			logException(e);
+		}
+		
+	}
+
+	private void logException(Exception e) {
+		Log.e(TAG,"Exception caught!",e);
+	}
+	
+	private boolean isNetworkAvailable() {
+		// Connectivity manager 
+		ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		//instantiate network info thing
+		//Go To permissions in AndroidManifest. Add ACCESS_NETWORK_STATE
+		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+		
+		boolean isAvailable = false;
+		if (networkInfo != null && networkInfo.isConnected()){
+			isAvailable = true;
+		}
+		
+		return isAvailable;
+	}
+
+	
+	public void handleBlogResponse() {
+		mProgressBar.setVisibility(View.INVISIBLE);
+		
+		if (mBlogData == null){
+			updateDisplayForError();
+			
+		}
+		else{
+			try {
+				JSONArray jsonPosts = mBlogData.getJSONArray("posts");
+				ArrayList<HashMap<String, String >> blogPosts = new ArrayList<HashMap<String, String>>();
+				for(int i = 0; i < jsonPosts.length(); i++){
+					JSONObject post = jsonPosts.getJSONObject(i);
+					String title = post.getString(KEY_TITLE);
+					//fix for especial characters...
+					title = Html.fromHtml(title).toString();
+					String author = post.getString(KEY_AUTHOR);
+					author = Html.fromHtml(author).toString();
+					
+					HashMap<String,String> blogPost = new HashMap<String,String>();
+					blogPost.put(KEY_TITLE, title);
+					blogPost.put(KEY_AUTHOR, author);
+					
+					blogPosts.add(blogPost);
+				}
+				
+				String[] keys = { KEY_TITLE, KEY_AUTHOR};
+				int[] ids = { android.R.id.text1, android.R.id.text2};
+				SimpleAdapter adapter = new SimpleAdapter(this, blogPosts,
+						android.R.layout.simple_list_item_2, keys, ids);
+				setListAdapter(adapter);
+				// Use this for the adapter view?? ----- - - - -- - -- - - -- - - -- - - - - -- 
+				
+			} catch (JSONException e) {
+				logException(e);
+			}
+		}
+	}
+
+	
+	//gets the title, error_message, ok button, and it creates i. and it shows it.
+	private void updateDisplayForError() {
+		//Dialog Display!! developer.android.com/guide/topics/ui/dialogs.html
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(getString(R.string.title));
+		builder.setMessage(getString(R.string.error_message));  // getString(R.string.something) from the string. file in res
+		builder.setPositiveButton(android.R.string.ok, null);
+		AlertDialog dialog = builder.create();
+		dialog.show();
+		
+		//
+		TextView emptyTextView = (TextView) getListView().getEmptyView();
+		emptyTextView.setText(getString(R.string.no_items));
+	}
+
+
+		//To Use Async --- go to developer.android.com/reference/os/asynctask.html
+	public class GetBlogPostsTask extends AsyncTask<Object, Void, JSONObject>{
+
+		@Override
+		protected JSONObject doInBackground(Object... arg0) {
+			int responseCode = -1;
+			JSONObject jsonResponse = null;
+			
+			try{
+				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count="
+						+ NUMBER_OF_POSTS);
+				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+				connection.connect();
+				
+				responseCode = connection.getResponseCode();				
+				if(responseCode == HttpURLConnection.HTTP_OK){
+					InputStream inputStream = connection.getInputStream();
+					Reader reader = new InputStreamReader(inputStream);
+					int contentLength = connection.getContentLength();
+					char[] charArray = new char[contentLength];
+					reader.read(charArray);					
+					String responseData = new String(charArray);				
+					//Log.v(TAG, responseData);
+		
+					jsonResponse = new JSONObject(responseData);
+					
+					
+				}else{
+					Log.i(TAG, "Unsuccessful HTTP Response Code: "+ responseCode);
+				}
+				Log.i(TAG, "Code: " + responseCode);
+			}
+			catch (MalformedURLException e){
+				logException(e);
+			}
+			catch (IOException e){
+				logException(e);
+			}
+			catch (Exception e){
+				logException(e);
+			}
+			return jsonResponse;
+		}
+		
+		@Override
+		protected void onPostExecute(JSONObject result){
+			mBlogData = result;
+			handleBlogResponse();
+		}
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	
+	
+}
